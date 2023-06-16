3c78cdd07169a579b329ad2fc549f6d8ff25190e
AmanDanidhariya <amandanidhariya007@gmail.com>
2023-06-16 20:13:45 +0530

    add data to repository

---
+++ src/components/AppNav.jsx
@@ -0,0 +1,13 @@
+
+import classes from './AppNav.module.css'
+const AppNav = () => {
+  return (
+    <>
+        <h1 className={classes.nav}>
+        AppNav
+        </h1>
+    </>
+  )
+}
+
+export default AppNav
---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,13 @@
+import AppNav from "../components/AppNav";
+import classes from "./AppLayout.module.css";
+
+const AppLayout = () => {
+  return (
+    <nav>
+    <AppNav/>
+      <h1>AppLayout</h1>
+    </nav>
+  );
+};
+
+export default AppLayout;
